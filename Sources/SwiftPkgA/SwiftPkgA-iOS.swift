cbe65a2485a4e64053a7b9454b7d76c90311efba
WooKyoung Noh <wookay.noh@gmail.com>
2021-02-18 00:17:57 +0900

    hello

--- Sources/SwiftPkgA/SwiftPkgA-iOS.swift
+++ Sources/SwiftPkgA/SwiftPkgA-iOS.swift
@@ -1,3 +1,7 @@
 #if os(iOS)
 
+public func hello() {
+    print("hello iOS")
+}
+
 #endif // #if os(iOS)
--- Sources/SwiftPkgA/SwiftPkgA-macOS.swift
+++ Sources/SwiftPkgA/SwiftPkgA-macOS.swift
@@ -1,3 +1,7 @@
 #if os(macOS)
 
+public func hello() {
+    print("hello macOS")
+}
+
 #endif // #if os(macOS)
